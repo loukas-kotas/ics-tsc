62fdc4a13a039172d2c286ace25fe684fb2cddae
loukas.kotas <loukas.kotas@gmail.com>
2019-06-23 20:52:49 +0300

    remove example

--- src/ics.ts
+++ src/ics.ts
@@ -176,18 +176,3 @@ END:VCALENDAR`
     }
 
 }
-
-// ----- EXAMPLE ------
-// BEGIN:VCALENDAR
-// VERSION:2.0
-// PRODID:http://www.icalmaker.com
-// BEGIN:VEVENT
-// UID:http://www.icalmaker.com/event/ed803b69-b846-49c4-a321-ccfc0ba55272
-// DTSTAMP:20190608T204202Z
-// DTSTART:20190609T052000Z
-// DTEND:20190610T063500Z
-// SUMMARY:test 1
-// LOCATION:Thessaloniki
-// DESCRIPTION:test 1 description
-// END:VEVENT
-// END:VCALENDAR
